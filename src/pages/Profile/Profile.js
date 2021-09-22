3452cecb645dea30b1181f47025712da58696eff
tayebehsafdari <66350369+tayebehsafdari@users.noreply.github.com>
2021-09-22 19:59:56 +0330

    modify files

--- src/pages/Profile/Profile.js
+++ src/pages/Profile/Profile.js
@@ -1,15 +1,39 @@
 import React from 'react';
 import {
     View,
-    Text
+    Text,
+    Image,
+    TouchableOpacity,
+    StyleSheet
 } from 'react-native';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import EStyleSheet from 'react-native-extended-stylesheet';
 
 const Profile = (props) => {
     return (
-        <View style={{flex: 1, backgroundColor: '#c0392b', justifyContent: 'center', alignItems: 'center'}}>
-            <Text>Profile Page</Text>
+        <View style={styles.container}>
+            <View style={{flexDirection: 'row', height: 150, paddingTop: 10, backgroundColor: 'red'}}>
+                <View style={styles._profileImage.container}>
+                    <Image source={require('./../../assets/images/reactnativelogo.jpg')}
+                           style={styles._profileImage.image}
+                    />
+                    <Ionicons
+                        name="md-add-circle"
+                        size={30}
+                        color="#3498db"
+                        style={styles._profileImage.icon}
+                    />
+                </View>
+                <View>
+                    <View></View>
+                    <View>
+                        <TouchableOpacity>
+                            <Text>ویرایش پروفایل</Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+            </View>
+            <View></View>
         </View>
     );
 }
@@ -23,7 +47,34 @@ Profile.navigationOptions = ({navigation}) => ({
     </View>
 });
 
-const styles = EStyleSheet.create({
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'white',
+        // justifyContent: 'center',
+        // alignItems: 'center'
+    },
+    _profileImage: {
+        container: {
+            flex: .3,
+            alignItems: 'center',
+            marginTop: 15
+        },
+        image: {
+            width: 80,
+            height: 80,
+            borderRadius: 40,
+            borderColor: '#888',
+            borderWidth: 2,
+            margin: 5
+        },
+        icon: {
+            bottom: 35,
+            left: 30,
+            borderRadius: 18,
+            overflow: 'hidden'
+        }
+    },
     profileName: {
         '@media ios': {
             fontFamily: 'IRANSansMobile',
