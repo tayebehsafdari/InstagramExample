3e122ef2a1993dc14435bf3cdea94472c676c9dc
tayebehsafdari <66350369+tayebehsafdari@users.noreply.github.com>
2021-09-22 13:40:57 +0330

    modify files

---
+++ src/assets/css/index.js
@@ -0,0 +1,13 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+    tabNavigatorProfileIcon: {
+        width: 26,
+        height: 26,
+        borderRadius: 13,
+        borderWidth: 1,
+        borderColor: '#333'
+    }
+});
+
+export default styles;
--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import type {Node} from 'react';
-import {Text, I18nManager} from 'react-native';
+import {Image, I18nManager} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 
-import OriginalStyle from './../assets/css';
+import styles from './../assets/css';
 
 import Camera from "../pages/Camera";
 import Follow from "../pages/Follow";
@@ -23,31 +23,46 @@ const App: ()  =>Node = () => {
     return (
         <NavigationContainer>
             <Tab.Navigator
-                screenOptions={({route}) => ({
-                    tabBarIcon: ({focused, color, size}) => {
-                        let iconName;
-                        if (route.name === 'Camera') {
-                            iconName = focused ? '' : ''
-                        } else if (route.name === 'Follow') {
-                            iconName = focused ? '' : ''
-                        } else if (route.name === 'HomeUser') {
-                            iconName = focused ? '' : ''
-                        } else if (route.name === 'Profile') {
-                            iconName = focused ? '' : ''
-                        } else if (route.name === 'Search') {
-                            iconName = focused ? '' : ''
-                        }
-                        return <Ionicons name={iconName} size={size} color={color}/>;
-                    },
-                    tabBarActiveTintColor: '',
-                    tabBarInactiveTintColor: ''
-                })}
+                screenOptions={}
             >
-                <Tab.Screen name="Camera" component={Camera}/>
-                <Tab.Screen name="Follow" component={Follow}/>
-                <Tab.Screen name="HomeUser" component={HomeUser}/>
-                <Tab.Screen name="Profile" component={Profile}/>
-                <Tab.Screen name="Search" component={Search}/>
+                <Tab.Screen
+                    name="Camera"
+                    component={Camera}
+                    options={{
+                        tabBarIcon: <Ionicons name="" size="" color=""/>
+                    }}
+                />
+                <Tab.Screen
+                    name="Follow"
+                    component={Follow}
+                    options={{
+                        tabBarIcon: <Ionicons name="" size="" color=""/>
+                    }}
+                />
+                <Tab.Screen
+                    name="HomeUser"
+                    component={HomeUser}
+                    options={{
+                        tabBarIcon: <Ionicons name="" size="" color=""/>
+                    }}
+                />
+                <Tab.Screen
+                    name="Profile"
+                    component={Profile}
+                    options={{
+                        tabBarIcon: <Image
+                            source={require('./../assets/images/reactnative.png')}
+                            style={styles.tabNavigatorProfileIcon}
+                        />
+                    }}
+                />
+                <Tab.Screen
+                    name="Search"
+                    component={Search}
+                    options={{
+                        tabBarIcon: <Ionicons name="" size="" color=""/>
+                    }}
+                />
             </Tab.Navigator>
         </NavigationContainer>
     );
